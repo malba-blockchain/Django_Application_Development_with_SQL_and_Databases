1864b9e00740ce742056f94de6857d8af49d3f02
Carlos Mario <cm.alba10@uniandes.edu.co>
2025-06-11 07:36:55 -0500

    finished first ORM lab

--- 2.1. Create Django Model/lab1_template/settings.py
+++ 2.1. Create Django Model/lab1_template/settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'P3otJ9AdeVjqSdEpJou8Iprp#',
+        'PASSWORD': 'Iiw4B0fHkulqnVZDl0ULOU4X',
         'HOST': 'postgres',
         'PORT': '5432',
     }
