1b4939469857628b3703faada7e10fdef13576f8
Carlos Mario <cm.alba10@uniandes.edu.co>
2025-06-13 08:16:33 -0500

    finished lab

---
+++ 2.4. Query Spanning Relationships/lab3_template/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'If0ZXK2sivH3cF68sbb0LvCi',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'related_objects',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
